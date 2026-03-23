e8816ee9ab691c0b2ef2414792802c2c09ba22ec
unknown <wendersoon010@gmail.com>
2026-03-23 15:37:08 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/wendersondev/springboot_jpa_workshop/config/TestConfig.java
+++ src/main/java/com/wendersondev/springboot_jpa_workshop/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.wendersondev.springboot_jpa_workshop.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.wendersondev.springboot_jpa_workshop.entities.Order;
 import com.wendersondev.springboot_jpa_workshop.entities.User;
+import com.wendersondev.springboot_jpa_workshop.repositories.OrderRepository;
 import com.wendersondev.springboot_jpa_workshop.repositories.UserRepository;
 
 @Configuration
@@ -16,6 +19,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,7 +29,12 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Ana", "ana@gmail.com", "987654321","123456");
 		User u2 = new User(null, "Alice", "alice@gmail.com", "198765432","123456");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
 	}
 
