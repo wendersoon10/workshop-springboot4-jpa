888e52abfbd8c605e2858bbb7445c8cd90ffb276
unknown <wendersoon010@gmail.com>
2026-03-27 10:58:26 -0300

    Exception handling - findById

--- src/main/java/com/wendersondev/springboot_jpa_workshop/services/UserService.java
+++ src/main/java/com/wendersondev/springboot_jpa_workshop/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.wendersondev.springboot_jpa_workshop.entities.User;
 import com.wendersondev.springboot_jpa_workshop.repositories.UserRepository;
+import com.wendersondev.springboot_jpa_workshop.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
